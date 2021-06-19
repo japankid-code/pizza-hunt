83c17eb5ffc4152c93058bb7b544ad88833f8a60
japankid-code <japankid.jake@gmail.com>
2021-06-19 15:32:03 -0500

    add pizza model, controller, and route

---
+++ models/Pizza.js
@@ -0,0 +1,26 @@
+// import only Schema and model from the library
+const { Schema, model } = require('mongoose')
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
+})
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
